f03a86d0aa101fc957949b7f354927de435283ba
vibhavanand <vibhavanand210@gmail.com>
2021-05-30 04:33:16 +0530

    added dockerfile and changed the folder structure

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,14 +1,16 @@
 from sqlalchemy import *
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref,sessionmaker
+from sqlalchemy.orm import relationship, backref, sessionmaker
 import jwt
 
 from sqlalchemy_utils import database_exists, create_database
 
 # base case for SQLalchemy ORM
 Base = declarative_base()
+
 
 class User(Base):
 
@@ -18,35 +20,35 @@ class User(Base):
     username = Column(String(122))
     password = Column(String(122))
     authentication_token = Column(String(1222))
-    def __init__(self, name, password,authentication_token):
+
+    def __init__(self, name, password, authentication_token):
         self.username = name
         self.password = password
-        self.authentication_token=authentication_token
+        self.authentication_token = authentication_token
 
 
 ########################################################################
-def connect_sql_db(sql_ip_port,username,pwd,db):
-    #params:
+def connect_sql_db(sql_ip_port, username, pwd, db):
+    # params:
     # sql_ip_port :(ip:port) ip and port of sqldb
     # username : username of db user
     # pwd : password
-    # db : database name  
+    # db : database name
     # cnx = create_engine('mysql+pymysql://root:@localhost:3306', pool_recycle=3600)
     connection_params = 'mysql+pymysql://'+username+':'+pwd+'@'+sql_ip_port+'/'+db
-    cnx = create_engine(connection_params,pool_recycle=3600)
-    #checking if db is present, if not creating the db
+    cnx = create_engine(connection_params, pool_recycle=3600)
+    # checking if db is present, if not creating the db
     if not database_exists(cnx.url):
         create_database(cnx.url)
         print('11111databasecreated')
     print('connection to db successful')
     return cnx
 
-def create_table(connection_db,Base_ , tb='registered_users'):
+
+def create_table(connection_db, Base_, tb='registered_users'):
     cnx = connection_db
-    #checking if table is present in db, if not creating it
-    if not cnx.dialect.has_table(cnx, tb):  # If table don't exist, Create.
+    inspect = sqlalchemy.inspect(cnx)
+    # checking if table is present in db, if not creating it
+    if not inspect.has_table(cnx, tb):  # If table don't exist, Create.
         Base_.metadata.create_all(cnx)
-        print('Table Creation Done')    
-
-
-
+        print('Table Creation Done')
---
+++ src/flask_app.py
@@ -0,0 +1,152 @@
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import sqlalchemy as sa
+from sqlalchemy import *
+from sqlalchemy.orm import scoped_session, sessionmaker
+from db_connection import *
+
+
+import pandas as pd
+import jwt
+
+# function for encoding and decoding auth tokens
+
+
+def encode_credentials_jwt(user_name, pwd):
+    encoded_jwt = jwt.encode({user_name: pwd}, 'secret', algorithm='HS256')
+    return encoded_jwt
+
+
+def decode_jwt(jwt_obtained):
+    decoded_jwt = jwt.decode(jwt_obtained, 'secret', algorithms=['HS256'])
+    user_name = list(decoded_jwt.keys())[0]
+    pwd = decoded_jwt[user_name]
+    return {user_name: pwd}
+# print(encoded_jwt,decoded_jwt.keys())
+
+
+# 2 signup api
+app = Flask(__name__)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        engine = connect_sql_db('localhost:3306', 'root', 'password', 'done')
+
+        # creating a session
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        create_table(engine, Base)
+        data = request.json
+        try:
+            user_name = data['username']
+            pwd = data['password']
+        except:
+            return jsonify({"success": "False", "msg": "Invalid Req Body"})
+
+        # checking if the username entered exists in the database
+        query = session.query(User).filter(User.username.in_([user_name]))
+        q = query.first()
+        # if user doesn't exist, creating a new user
+        if q is None:
+            print("no user present")
+            user_details = User(user_name, pwd, '')
+            session.add(user_details)
+            session.commit()
+            return jsonify({"success": "True", "msg": "User Created"})
+        # if user exists
+        else:
+            return jsonify({"success": "False", "msg": "User is already present, please use login instead"})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # hardcoded these, can be used from env file in dev environment later
+        engine = connect_sql_db('localhost:3306', 'root', 'password', 'done')
+        # creating a session
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        create_table(engine, Base)
+
+        try:
+            data = request.json
+            user_name = data['username']
+            pwd = data['password']
+        except:
+            return jsonify({"success": "False", "msg": "Invalid Req Parameters"})
+
+        # query to match if the given username is present in DB
+        query = session.query(User).filter(User.username.in_([user_name]))
+        q = query.first()
+
+        # if user is not present in DB, prompting him to make an account
+        if q is None:
+            print("no user present")
+            return jsonify({"success": "False", "msg": "User is not present. Please use Signup to create an account."})
+
+        # query to match given username, pwd combo
+        query = session.query(User).filter(
+            User.username.in_([user_name]), User.password.in_([pwd]))
+        q = query.first()
+
+        # if the combo doesn't match, prompting him with an error message.
+        if q is None:
+            return jsonify({"success": "False", "msg": "Username, Password combination does not match."})
+        # if verified, creating an access token,updating it in the DB to verify it in future and passing it to the client.
+        else:
+            print("user details verified, generating token.")
+
+            # first encoding the username pwd, then converting the byte object to string
+            authtoken = encode_credentials_jwt(user_name, pwd).decode("utf-8")
+            session.query(User).filter(User.username.in_(['adminn'])).update(
+                {'authentication_token': authtoken}, synchronize_session=False)
+            session.commit()
+            return jsonify({"success": "True", "msg": "user verified", "Access_token": authtoken})
+    if request.method == 'GET':
+        return jsonify({"message": "be a dear and send correct parameters"})
+
+
+@app.route('/auth_token', methods=['POST', 'GET'])
+def auth_token():
+    if request.method == 'POST':
+        data = request.json
+        engine = connect_sql_db('localhost:3306', 'root', '', 'done')
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # extracting auth token and converting it into a byte string for using the decode_jwt function.
+        try:
+            access_token_client = data['Access_token']
+        except:
+            return jsonify({"success": "False", "msg": "Invalid Req Parameters"})
+
+        # access_token_client=access_token_client
+
+        # string with encoding 'utf-8'
+        access_token_client = bytes(access_token_client, 'utf-8')
+
+        # checking if the token is valid
+        try:
+            credentials_details = decode_jwt(access_token_client)
+        except Exception as e:
+            return jsonify({"success": "False", "msg": "Invalid Token"})
+
+        user_name = list(credentials_details.keys())[0]
+        pwd = credentials_details[user_name]
+
+        # checking if the credentials obtained from decoding token match in our DB.
+        query = session.query(User).filter(User.username.in_([user_name]), User.password.in_(
+            [pwd]), User.authentication_token.in_([access_token_client]))
+        q = query.first()
+        if query is None:
+            return jsonify({"success": "False", "msg": "token data and credentials don't match"})
+
+        else:
+            return jsonify({"success": "True", "msg": 'token and crerdentials validated.'})
+
+
+app.run(host="0.0.0.0", port=int("5000"), debug=True)
